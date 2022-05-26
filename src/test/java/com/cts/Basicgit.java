4b7c4b2129d62626d253331b2037410fd9a9ecfb
virukshavijaykumar <virukshavijaykumar@gmail.com>
2022-05-26 14:50:04 +0530

    first time push

---
+++ src/test/java/com/cts/Basicgit.java
@@ -0,0 +1,17 @@
+package com.cts;
+
+public class Basicgit {
+
+	private static void add() {
+		System.out.println("Add");		
+	}
+	
+	private void sub() {
+		System.out.println("sub");	
+	}
+
+	private void mul() {
+		System.out.println("mul");	
+	}
+
+}
